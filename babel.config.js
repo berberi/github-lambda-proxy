744b707189ceb7470786cb9d0bb7d60b8be706fc
Alex Berberi <alexlberberi@gmail.com>
2019-08-20 09:09:43 -0400

    add babel config

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function(api) {
+  api.cache(true);
+
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          targets: { node: "10.16.3" }
+        }
+      ]
+    ],
+    plugins: []
+  };
+};
